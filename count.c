ecd904ece440c0c5de1c5e6a59d7196faec225bf
Peter Nemeth <fbpeter0@gmail.com>
2018-04-23 11:10:44 +0200

    b5

--- count.c
+++ count.c
@@ -6,13 +6,12 @@ int main()
 {
     printf("Hello world!\n");
     int age = 25;
-    int month = 4;
+    int year = 1992;
     count(age, month);
     return 0;
 }
 
-void count(int age, int month){
+void count(int age, int year){
     printf("My age is %d.\n", age);
-    printf("I was born in %dth month", month);
-
+    printf("I was born in %d", year);
 }
