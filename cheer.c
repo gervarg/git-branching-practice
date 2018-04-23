e7398fc8ed3a697688f16cee16d008ef799f2fe5
benjibraun <benjibraun@gmail.com>
2018-04-23 10:42:26 +0200

    c2

--- cheer.c
+++ cheer.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void cheer(char*);
 int main()
 {
-    printf("Hello embedded!\n");
+    char* team_name ="embedded";
+    cheer(team_name);
     return 0;
+}
+void cheer(char* team_name){
+
+ printf("Hello %s!\n",team_name);
 }
