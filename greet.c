b48dc188366dca4228bddb3807073b07fb912b47
Varga Gergely <reapedsoul@gmail.com>
2018-04-23 10:32:55 +0200

    a1

---
+++ greet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void greet(void);
+
+int main()
+{
+    printf("Hello branch!\n");
+    greet(void);
+    return 0;
+}
+
+void greet(void)
+{
+    printf("Hello Gergely!");
+}
+
