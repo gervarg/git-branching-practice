9af970b2fa93692f4e6dcec7162357d5460ab137
benjibraun <benjibraun@gmail.com>
2018-04-23 11:21:57 +0200

    c6

--- cheer.c
+++ cheer.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-void cheer(char*);
+void cheer(char*, char*);
 int main()
 {
-    char* team_name ="C";
-    cheer(team_name);
+    char* team_name = "embedded";
+    char* langue = "C";
+    cheer(team_name, langue);
     return 0;
 }
 void cheer(char* team_name ){
-
+ printf("Hello %s!\n",langue);
  printf("Chosen language is: %s!\n",team_name);
 }
