ccc28ff4a4af25e334fc885be9b457a6964290b5
Peter Nemeth <fbpeter0@gmail.com>
2018-04-23 10:46:06 +0200

    b3

--- cheer.c
+++ cheer.c
@@ -3,11 +3,11 @@
 void cheer(char*);
 int main()
 {
-    char* team_name ="embedded";
+    char* team_name ="C";
     cheer(team_name);
     return 0;
 }
 void cheer(char* team_name){
 
- printf("Hello %s!\n",team_name);
+ printf("Chosen language is: %s!\n",team_name);
 }
