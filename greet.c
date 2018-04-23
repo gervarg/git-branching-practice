534d5fa4e2eba9ce7a2157fe9f2c321f0547124e
Varga Gergely <reapedsoul@gmail.com>
2018-04-23 11:12:05 +0200

    added new param and printed out

--- greet.c
+++ greet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void greet(char*);
+void greet(char*, int);
 
 int main()
 {
     char *name = "Gergely";
+    int age = 34;
     printf("Hello branch!\n");
     greet(name);
     return 0;
 }
 
-void greet(char* name)
+void greet(char* name, int age)
 {
-    printf("Greetings tesa %s!\n", name);
+    printf("Greetings tesa %s, I am %d years old!\n", name, age);
 }
 
