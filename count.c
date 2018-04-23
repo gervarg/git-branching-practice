481809ea1cf53d2a5b83880efbc111e3a642e984
Varga Gergely <reapedsoul@gmail.com>
2018-04-23 10:48:06 +0200

    a3

--- count.c
+++ count.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-void count(int);
+void count(int, int);
 
 int main()
 {
     printf("Hello world!\n");
-    int age 25;
-    count(age);
+    int age = 25;
+    int month = 4;
+    count(age, month);
     return 0;
 }
 
-void count(int age){
+void count(int age, int month){
     printf("My age is %d.\n", age);
+    printf("I was born in %dth month", month);
+
 }
