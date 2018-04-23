00425e8300631192942c7f9766667ab89eac485e
Varga Gergely <reapedsoul@gmail.com>
2018-04-23 10:13:22 +0200

    a1

--- Hello_branch/main.c
+++ Hello_branch/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void greet(void);
+
 int main()
 {
     printf("Hello branch!\n");
+    greet(void);
     return 0;
+}
+
+void greet(void)
+{
+    printf("Hello Gergely!");
 }
