11aef61c6cf17432c88df68f8f37dae11d0ad2f6
Peter Nemeth <fbpeter0@gmail.com>
2018-04-23 10:31:36 +0200

    b1

---
+++ count.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+void count(void);
+
+int main()
+{
+    printf("Hello world!\n");
+    count();
+    return 0;
+}
+
+void count(void){
+    printf("My age is 25.\n");
+}
